c5d6711baa87023ff1f14225f802cb93a639f0dc
Redhawk18 <redhawk76767676@gmail.com>
2023-05-28 20:30:37 -0400

    created folder for gui code

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,9 @@
-use iced::widget::TextInput;
-use iced::{theme, Application, Command, Element, Settings, Subscription};
+use iced::{Application, Settings};
+
+mod gui;
+
+pub use gui::gui::ApplicationState;
 
 fn main() -> iced::Result {
     ApplicationState::run(Settings::default())
-}
-
-#[derive(Debug, Clone)]
-pub enum Message {
-    Update(String),
-}
-
-struct ApplicationState {
-    text: String,
-}
-
-impl Application for ApplicationState {
-    type Executor = iced::executor::Default;
-
-    type Message = Message;
-
-    type Theme = theme::Theme;
-
-    type Flags = ();
-
-    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (
-            ApplicationState {
-                text: String::from(""),
-            },
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        String::from("code editor")
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::Update(new_text) => {
-                self.text = new_text;
-            }
-        }
-
-        Command::none()
-    }
-
-    fn view(&self) -> Element<Message> {
-        Element::new(TextInput::new("", &self.text).on_input(|new_text| Message::Update(new_text)))
-    }
-
-    fn subscription(&self) -> Subscription<Message> {
-        Subscription::none()
-    }
 }
