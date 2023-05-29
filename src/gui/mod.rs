65a92d51733a3676de4256d458f021a29222e7af
Redhawk18 <redhawk76767676@gmail.com>
2023-05-28 20:54:24 -0400

    removed nested namespace

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,1 +1,52 @@
-pub mod gui;
+use iced::widget::TextInput;
+use iced::{theme, Application, Command, Element, Subscription};
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Update(String),
+}
+
+pub struct ApplicationState {
+    text: String,
+}
+
+impl Application for ApplicationState {
+    type Executor = iced::executor::Default;
+
+    type Message = Message;
+
+    type Theme = theme::Theme;
+
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (
+            ApplicationState {
+                text: String::from(""),
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("code editor")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Update(new_text) => {
+                self.text = new_text;
+            }
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        Element::new(TextInput::new("", &self.text).on_input(Message::Update))
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Subscription::none()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use iced::{Application, Settings};
 
 mod gui;
 
-pub use gui::gui::ApplicationState;
+pub use gui::ApplicationState;
 
 fn main() -> iced::Result {
     ApplicationState::run(Settings::default())
