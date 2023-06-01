8f2ba667f5e94943f97db0c9192e9ccfaea18a6e
Redhawk18 <redhawk76767676@gmail.com>
2023-06-01 10:47:47 -0400

    added new option in the menu bar

--- src/gui/menu_bar.rs
+++ src/gui/menu_bar.rs
@@ -2,7 +2,20 @@ use iced::widget::button;
 use iced_aw::menu::{MenuBar, MenuTree};
 
 pub fn file<'a>(_app: &super::State) -> MenuTree<'a, super::Message, iced::Renderer> {
-    let root = MenuTree::new(button("File",));
+    let open_file = MenuTree::new(button("Open File",)); 
+
+    let open_folder = MenuTree::new(button("Open Folder",));
+
+    let save = MenuTree::new(button("Save",));
+
+    let save_as = MenuTree::new(button("Open File",));
+
+    let root = MenuTree::with_children(button("File"), vec![
+        open_file,
+        open_folder,
+        save,
+        save_as,
+    ]);
 
     root
 }
