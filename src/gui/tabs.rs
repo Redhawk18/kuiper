7acae9153f7c1be0512952a74bb94bc90387b8b7
Redhawk18 <redhawk76767676@gmail.com>
2023-07-07 16:20:10 -0400

    displays filename if it exists

--- src/gui/tabs.rs
+++ src/gui/tabs.rs
@@ -1,14 +1,19 @@
-use iced::Element;
 use iced::widget::row;
+use iced::Element;
 use iced_aw::{TabBar, TabLabel};
 
 pub fn tab_header(tabs: &[super::FileTab], active_tab: usize) -> Element<super::Message> {
     let mut tab_bar =
         TabBar::new(active_tab, super::Message::TabSelected).on_close(super::Message::TabClosed);
 
-    for _tab in tabs.iter() {
-        //TODO if let some tab.path is "" else name it new tab
-        tab_bar = tab_bar.push(TabLabel::Text(String::from("tab name!")));
+    for tab in tabs.iter() {
+        let filename = tab
+            .path
+            .file_name() // this already checks the "empty" case
+            .and_then(|filename| filename.to_str())
+            .unwrap_or("New Tab");
+
+        tab_bar = tab_bar.push(TabLabel::Text(String::from(filename)));
     }
 
     row!(tab_bar).into()
