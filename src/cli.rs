3654cc59606a1d878f06440ef3203c8cacd0b30c
Redhawk18 <redhawk76767676@gmail.com>
2023-09-19 22:11:46 -0400

    use crate infomation

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,18 @@
 use blaze_gui::start_gui;
-use clap::Command;
+use clap::{crate_authors, crate_description, crate_name, crate_version, Command};
 
 pub fn create_command() {
-    let matches = Command::new(env!("CARGO_PKG_NAME"))
-        .about("Hybrid integrated development environment")
-        .subcommand_required(true)
-        .version(env!("CARGO_PKG_VERSION"))
+    let matches = Command::new(crate_name!())
+        .about(crate_description!())
+        .author(crate_authors!())
         .subcommand(
             Command::new("gui")
                 .about("Start the gui frontend")
                 .long_flag("gui")
                 .short_flag('g'),
         )
+        .subcommand_required(true)
+        .version(crate_version!())
         .get_matches();
 
     match matches.subcommand() {
