ba44b3d9cb2b615ada60e757fa032e1b3dc87327
Redhawk18 <redhawk76767676@gmail.com>
2023-09-25 22:18:39 -0400

    theme changes

--- tui/src/theme.rs
+++ tui/src/theme.rs
@@ -7,21 +7,20 @@ use cursive::{
 pub fn theme(siv: &Cursive) -> Theme {
     let mut theme = siv.current_theme().clone();
 
-    // tui's have no concept of light and dark mode
-    let palette = Palette::new(true);
+    // tui's have no concept of light and dark mode reconsider when config files happen TODO 
+    let palette = Palette::new(false);
 
-    theme.palette[PaletteColor::Background] = Color::TerminalDefault;
-
+    theme.palette[PaletteColor::Background] = to_color(palette.background.default);
     theme.palette[PaletteColor::Highlight] = to_color(palette.accent.default);
     theme.palette[PaletteColor::HighlightInactive] = to_color(palette.secondary.default);
-    theme.palette[PaletteColor::HighlightText] = to_color(palette.text.default);
+    theme.palette[PaletteColor::HighlightText] = to_color(palette.background.default);
     theme.palette[PaletteColor::Primary] = to_color(palette.text.default);
-    theme.palette[PaletteColor::Secondary] = to_color(palette.text.default);
+    theme.palette[PaletteColor::Secondary] = to_color(palette.text.lighter);
     theme.palette[PaletteColor::Shadow] = to_color(palette.secondary.default);
-    theme.palette[PaletteColor::Tertiary] = to_color(palette.text.default);
-    theme.palette[PaletteColor::TitlePrimary] = to_color(palette.text.default);
-    theme.palette[PaletteColor::TitleSecondary] = to_color(palette.text.default);
-    theme.palette[PaletteColor::View] = to_color(palette.primary.default);
+    theme.palette[PaletteColor::Tertiary] = to_color(palette.text.lightest);
+    theme.palette[PaletteColor::TitlePrimary] = to_color(palette.accent.default);
+    theme.palette[PaletteColor::TitleSecondary] = to_color(palette.primary.default);
+    theme.palette[PaletteColor::View] = to_color(palette.background.default);
     theme
 }
 
