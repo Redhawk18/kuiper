22e193204cd9fd152a46a38af9793d42c0525793
Redhawk18 <redhawk76767676@gmail.com>
2023-11-15 15:33:16 -0500

    proper workspace support

--- tui/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub fn start_tui() {
-    println!("Not implemented, YET");
-    println!("I'm making my own tui crate since none of the ones out there really support or allow me to make the ui i want");
-    println!("It's a lot harder than just making a pr to add the features i want");
-    println!("The problem with the other tui crates is core to their design, ie state management and not being able to update the ui");
-    println!("stay tuned, this is just a roadblock for now");
-}
